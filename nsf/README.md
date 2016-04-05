9eb4db5281c54e29036957ab826b2629d4469daf
Bluenex <tulakan-@hotmail.com>
2016-04-05 23:15:43 +0700

    fill bash to download and unzip

--- nsf/README.md
+++ nsf/README.md
@@ -2,5 +2,19 @@
 
 All NSF award can be downloaded [here](https://www.nsf.gov/awardsearch/download.jsp).
 
-run `python download_award_links.py` to create `nsf_awards.txt` which contain
-link to zip file that we can download later.
+Run
+
+```
+./dl_nsf.sh
+```
+
+If you are not able to run then do `chmod` first
+
+```
+chmod +x dl_nsf.sh
+```
+
+and run again
+
+This script will run `python download_award_links.py` to create `nsf_awards.txt` which contains
+link to zip file. Then it downloads and unzips all those zip files into `data` directory.
---
+++ nsf/dl_nsf.sh
@@ -0,0 +1,23 @@
+python download_award_links.py # load to nsf
+
+# check whether there is already data folder or not
+if [ ! -d $PWD/data ]; then
+  mkdir $PWD/data
+fi
+
+# download all
+while read p; do
+  # echo $p | egrep -o [0-9]+ # extract only number but there is also
+  # https://www.nsf.gov/awardsearch/download?DownloadFileName=2009&All=true
+  # echo grep -oP '(?<=DownloadFileName=).*?(?=&)' <<< "$p" # -P doesn't work on Mac anymore
+  wget -O data/$(echo $p | awk -F'[=&]' '{print $2}').zip $p
+done <nsf_awards.txt
+
+# unzip all files
+for z in $PWD/data/*
+do
+  unzip -q $z -d ${z%.*} # remove q for verbose output
+  echo unzipped ${z##*/}
+done
+
+# and parse later
