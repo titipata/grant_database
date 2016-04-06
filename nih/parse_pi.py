b1f15c0c1f4b4b4ff34ee44217d8ac80dce94f76
titipata <titipat.a@u.northwestern.edu>
2016-04-05 23:47:10 -0500

    Append PI ID to output from NIH grant

--- nih/parse_pi.py
+++ nih/parse_pi.py
@@ -1,6 +1,18 @@
+import re
 import glob
 from itertools import chain
 import pandas as pd
+
+
+def remove_contact_str(text):
+    """Remove string '(contact)' from """
+    return re.sub('\(contact\)', '', text).strip()
+
+
+def process_pi_id(pi_id):
+    """Transform PI ids in NIH format to list"""
+    pi_ids = [n for n in map(lambda s: s.strip(), pi_id.split(';')) if n != '']
+    return list(map(remove_contact_str, pi_ids))
 
 
 def add(d, k, v):
@@ -28,11 +40,17 @@ def process_pi_name(pi_name):
 if __name__ == '__main__':
     # example of getting one author table from year 2015
     path_list = glob.glob('data/proj/*/*.csv', recursive=True)
-    print("Extract authors from %s" % path_list[30].split('/')[-1])
+    print("Extract authors from %s ..." % path_list[30].split('/')[-1])
     df = pd.read_csv(path_list[30], encoding = "ISO-8859-1")
-    pi_dicts = zip(df.APPLICATION_ID, df.PI_NAMEs.map(lambda n: process_pi_name(n)))
-    pi_dicts = [list(map(lambda d: add(d, 'application_id', application_id), d)) for (application_id, d) in pi_dicts]
-    pi_dicts = list(chain(*pi_dicts)) # flatten list of
-    pi_df = pd.DataFrame(pi_dicts)
+    pi_dicts = zip(df.APPLICATION_ID,
+                   df.PI_IDS.map(lambda idx: process_pi_id(idx)),
+                   df.PI_NAMEs.map(lambda n: process_pi_name(n)))
+    pi_dicts_all = []
+    for (application_id, pi_id, d) in pi_dicts:
+        d = [add(d, 'pi_id', i) for (i, d) in zip(pi_id, d)] # add pi_id to dict
+        d = list(map(lambda d: add(d, 'application_id', application_id), d)) # add application_id to dict
+        pi_dicts_all.append(d)
+    pi_dicts_all = list(chain(*pi_dicts_all)) # flatten list of list
+    pi_df = pd.DataFrame(pi_dicts_all)
     pi_df.to_csv('nih_grant_investigators.csv', index=False)
     print("Finish saving to csv file")
